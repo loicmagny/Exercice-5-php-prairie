e41cb37a3466f3414da2863899673e62f139d421
loicmagny <loicmagn60@gmail.com>
2017-10-02 10:59:06 +0200

    exercice5

---
+++ index5.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <?php
+  $phrase1 = "Quelle partie du légume ne passe pas au mixeur?";
+  $phrase2 = "Le fauteuil.";
+  $fusion = "$phrase1 "."$phrase2";
+  echo strrev("$fusion");
+  ?>
+</body>
+</html>
